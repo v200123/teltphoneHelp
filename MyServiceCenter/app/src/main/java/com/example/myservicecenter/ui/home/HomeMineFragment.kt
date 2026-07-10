74cdb98f61ca636eefaf7f775484e9f3b2254715
295799682@qq.com <laptoy543621187@qq.com>
2026-07-10 17:31:40 +0800

    让我的更加逼近真实APP

--- MyServiceCenter/app/src/main/java/com/example/myservicecenter/ui/home/HomeMineFragment.kt
+++ MyServiceCenter/app/src/main/java/com/example/myservicecenter/ui/home/HomeMineFragment.kt
@@ -86,6 +86,9 @@ class HomeMineFragment : Fragment(R.layout.fragment_home_mine) {
         Glide.with(this)
             .load("https://res.app.coc.10086.cn/qwhdcdn_cmcc-cs_cn/prd-mgcenter/c587d3a308314e34aa7885d8f37f8d9a.png?fmt=webp&width=288&height=80")
             .into(binding.ivMineServiceCenter)
+        Glide.with(this)
+            .load("https://res.app.coc.10086.cn/qwhdcdn_cmcc-cs_cn/prd-mgcenter/c33ccbb49eba49d0b5a2aac871de5527.png?fmt=webp")
+            .into(binding.ivHomeDeviceTrade)
 
         Glide.with(this).load("https://res.app.coc.10086.cn/qwhdcdn_cmcc-cs_cn/prd-mgcenter/29a5ceb71fcf48daa2ccf2feb3d8b93d.gif").into(binding.floatWindowImg)
         renderMineComboItems()
